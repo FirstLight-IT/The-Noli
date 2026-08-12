8534dee26be3272cb5bb04a669c1cda1bd4652ee
Carl Dominise <dominise.jann_carl_myko@hnu.edu.ph>
2026-08-12 20:40:32 +0800

    Version 0.36 | Added Initial Android Setup and Controls

--- Assets/Scripts/Characters/MobileController.cs
+++ Assets/Scripts/Characters/MobileController.cs
@@ -2,9 +2,13 @@ using UnityEngine;
 
 public class MobileController : MonoBehaviour
 {
-    
     void Start()
     {
+        if (Application.isMobilePlatform)
+        {
+            Application.targetFrameRate = 60;
+        }
+
         gameObject.SetActive(Application.isMobilePlatform);
     }
 }
